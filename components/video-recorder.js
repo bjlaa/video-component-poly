0e123a7218132c1822326b264d66550b56ae2c78
Benjamin <benjamin.j.arias@gmail.com>
2016-06-14 15:20:58 +0200

    feat: Created video-recorder component capturing video enabled and live feeding to app enabled

--- components/video-recorder.js
+++ components/video-recorder.js
@@ -1,19 +1,66 @@
 import React from 'react';
 
 var VideoRecorder = React.createClass({
-	componentDidMount: function() {
+	renderVideo() {
+		this.refs.video.style.visibility = 'visible';
+		this.refs.button.style.display = 'none';
+		this.captureVideoAudio();
+	},
+
+	render: function() {
+		var renderedComponent;
+		return(
+			<div>
+				<div className='record'>
+					<div className='record-message'>Let's try recording a video:</div>
+					<div ref='button' className='record-button-container'>
+						<i onClick={this.renderVideo} className="fa fa-video-camera" aria-hidden="true"></i>
+						<i className="fa fa-arrow-up" aria-hidden="true"></i>
+						<div className='record-click-me' >Click me!</div>
+					</div>
+				</div>					
+				<div ref='video' id='video-container'>
+					<video id='camera-stream' width='500' autoPlay ></video>
+				</div>				
+			</div>
+			
+		)
+	},
+
+	captureVideoAudio: function() {
 	  navigator.getUserMedia = (navigator.getUserMedia ||
 	                            navigator.webkitGetUserMedia ||
 	                            navigator.mozGetUserMedia || 
 	                            navigator.msGetUserMedia);
-	},
+		if (navigator.getUserMedia) {
+		  // Request the camera.
+		  navigator.getUserMedia(
+		    // Constraints
+		    {
+		      video: true,
+		      audio: true
+		    },
 
-	render: function() {
-		return(
-			<div id='video-container'>
-				<video id='camera-stream' width='500' autoplay></video>
-			</div>
-		)
+		    // Success Callback
+		    function(localMediaStream) {
+					// Get a reference to the video element on the page.
+					var vid = document.getElementById('camera-stream');
+
+					// Create an object URL for the video stream and use this 
+					// to set the video source.
+					vid.src = window.URL.createObjectURL(localMediaStream);
+		    },
+
+		    // Error Callback
+		    function(err) {
+		      // Log the error to the console.
+		      console.log('The following error occurred when trying to use getUserMedia: ' + err);
+		    }
+		  );
+
+		} else {
+		  alert('Sorry, your browser does not support getUserMedia');
+		}		
 	}
 });
 
