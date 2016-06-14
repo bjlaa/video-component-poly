1ed6f082e87e915e2d8cf99cdf82caf8253aa634
Benjamin <benjamin.j.arias@gmail.com>
2016-06-14 13:04:12 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+var source = require('vinyl-source-stream');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var watchify = require('watchify');
+var notify = require('gulp-notify');
+
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var buffer = require('vinyl-buffer');
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var historyApiFallback = require('connect-history-api-fallback')
+
+
+/*
+  Styles Task
+*/
+
+gulp.task('styles', function() {
+  return gulp.src('./sass/styles.scss')
+    .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(reload({stream:true}))
+});
+
+gulp.task('watch', function(){
+  gulp
+  .watch('./sass/**/*.scss', ['styles'])
+  .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+
+});
+
+
+/*
+  Browser Sync
+*/
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        // we need to disable clicks and forms for when we test multiple rooms
+        server : {},
+        middleware : [ historyApiFallback() ],
+        ghostMode: false
+    });
+});
+
+function handleErrors() {
+  var args = Array.prototype.slice.call(arguments);
+  notify.onError({
+    title: 'Compile Error',
+    message: '<%= error.message %>'
+  }).apply(this, args);
+  this.emit('end'); // Keep gulp from hanging on this task
+}
+
+function buildScript(file, watch) {
+  var props = {
+    entries: ['./scripts/' + file],
+    debug : true,
+    transform:  [babelify.configure({stage : 0 })]
+  };
+
+  // watchify() if watch requested, otherwise run browserify() once 
+  var bundler = watch ? watchify(browserify(props)) : browserify(props);
+
+  function rebundle() {
+    var stream = bundler.bundle();
+    return stream
+      .on('error', handleErrors)
+      .pipe(source(file))
+      // If you also want to uglify it
+      // .pipe(buffer())
+      // .pipe(uglify())
+      // .pipe(rename('app.min.js'))
+      // .pipe(gulp.dest('./build'))
+      .pipe(gulp.dest('./build/'))
+      .pipe(reload({stream:true}))
+  }
+
+  // listen for an update and run rebundle
+  bundler.on('update', function() {
+    rebundle();
+    gutil.log('Rebundle...');
+  });
+
+  // run it once the first time buildScript is called
+  return rebundle();
+}
+
+gulp.task('scripts', function() {
+  return buildScript('main.js', false); // this will run once because we set watch to false
+});
+
+// run 'scripts' task first, then watch for future changes
+gulp.task('default', ['styles','scripts','browser-sync'], function() {
+  gulp.watch('./sass/**/*.scss', ['styles']);
+  return buildScript('main.js', true); // browserify watch for JS changes
+});
---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+var glawie = 2;
