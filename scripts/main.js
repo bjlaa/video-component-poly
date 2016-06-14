feda195edc75c5733be7287dcc3f9896a35eb60d
Benjamin <benjamin.j.arias@gmail.com>
2016-06-14 13:08:02 +0200

    feat: Created container for app - App component

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
-var glawie = 2;
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+	render() {
+		return(
+			<div className='app'>
+				<div className='title'>Test Video Component</div>
+				
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('main'));
