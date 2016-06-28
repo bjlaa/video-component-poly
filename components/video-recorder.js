95f23583f145f4dddae9a72dc0859d1ba306a33a
Benjamin <benjamin.j.arias@gmail.com>
2016-06-28 15:42:57 +0200

    feat: Added record + stop buttons + save video to client

--- components/video-recorder.js
+++ components/video-recorder.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import RecordRTC from 'recordrtc';
 
+// Constraints for our video capture/recording
+var mediaConstraints = { video: true, audio: true }; 
+var recordRTC;
+var urlVideo;
+
+
 var VideoRecorder = React.createClass({
 	componentDidMount: function() {
 
@@ -53,9 +59,11 @@ var VideoRecorder = React.createClass({
 						<div className='record-click-me' >Click me!</div>
 					</div>
 				</div>					
-				<div ref='video' id='video-container' muted>
-					<video id='camera-stream' width='500' autoPlay ></video>
-					<button onClick={this.recordVideo} className='button-record'>Record</button>
+				<div ref='video' id='video-container' >
+					<video id='camera-stream' width='500' autoPlay muted></video>
+					<button ref='button-record'onClick={this.recordVideo} className='button-record'>Record</button>
+					<button ref='button-stop' onClick={this.stopRecording} className='button-stop' >Stop</button>
+					<div ref= 'button-download' id='button-download'></div>
 					<div>
 			      <label className='labels-upload' htmlFor="title-upload">Title:</label>
 			      <input id="title-upload" type="text" defaultValue=''/>
@@ -79,7 +87,69 @@ var VideoRecorder = React.createClass({
 	},
 
 	recordVideo: function() {
-		console.log('Video recording started!')
+	  navigator.getUserMedia(
+	    // Constraints
+	    mediaConstraints,
+
+	    // Success Callback
+	    function(localMediaStream) {
+				//RecordRTC part - recording of the video
+
+				// Get a reference to the video element on the page.
+				var video = document.getElementById('camera-stream');
+
+				var options = {
+					mimeType: 'video/webm',
+					audioBitsPerSecond: 128000,
+					videoBitsPerSecond: 128000,
+					bitsPerSecond: 128000
+				};
+				recordRTC = RecordRTC(localMediaStream, options);
+				recordRTC.startRecording();
+				console.log('Recording started!');
+	    },
+
+	    // Error Callback
+	    function(err) {
+	      // Log the error to the console.
+	      console.log('The following error occurred when trying to use getUserMedia: ' + err);
+	    }
+	  );	
+	},
+
+	stopRecording: function() {
+	  navigator.getUserMedia(
+	    // Constraints
+	    mediaConstraints,
+
+	    // Success Callback
+	    function(localMediaStream) {
+
+				// Get a reference to the video element on the page.
+				var video = document.getElementById('camera-stream');
+
+				//RecordRTC part - recording of the video
+				recordRTC.stopRecording(function(audioVideoWebMURL) {
+					video.src = audioVideoWebMURL;
+
+					var recordedBlob = recordRTC.getBlob();
+					recordRTC.getDataURL(function(dataURL) { });
+					
+					var downloadURL = document.getElementById('button-download');
+					var url = video.src;
+					video.muted = false;
+					video.play();
+					downloadURL.innerHTML = '<a href="' + url + '" download="video.webm" target="_blank">Save Video</a><hr>';
+
+				});
+	    },
+
+	    // Error Callback
+	    function(err) {
+	      // Log the error to the console.
+	      console.log('The following error occurred when trying to use getUserMedia: ' + err);
+	    }
+	  );
 	},
 
 	captureVideoAudio: function() {
@@ -91,19 +161,19 @@ var VideoRecorder = React.createClass({
 		  // Request the camera.
 		  navigator.getUserMedia(
 		    // Constraints
-		    {
-		      video: true,
-		      audio: true
-		    },
+		    mediaConstraints,
 
 		    // Success Callback
 		    function(localMediaStream) {
+
+		    	//Rendering video on screen part
+
 					// Get a reference to the video element on the page.
-					var vid = document.getElementById('camera-stream');
+					var video = document.getElementById('camera-stream');
 
 					// Create an object URL for the video stream and use this 
 					// to set the video source.
-					vid.src = window.URL.createObjectURL(localMediaStream);
+					video.src = window.URL.createObjectURL(localMediaStream);
 		    },
 
 		    // Error Callback
@@ -111,9 +181,7 @@ var VideoRecorder = React.createClass({
 		      // Log the error to the console.
 		      console.log('The following error occurred when trying to use getUserMedia: ' + err);
 		    }
-		  )
-		  .then(successCallback)
-		  .catch(errorCallback);
+		  );
 
 		} else {
 		  alert('Sorry, your browser does not support getUserMedia');
